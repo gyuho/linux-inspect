7f39ec7e57f1a57a6a434c3c392031a718de849a
Gyu-Ho Lee <gyuhox@gmail.com>
2016-01-05 21:48:08 -0800

    change command var names

--- main.go
+++ main.go
@@ -23,18 +23,18 @@ var (
 	w          = os.Stdout
 	globalFlag = GlobalFlag{GlobalFilter: &ss.Process{}}
 
-	rootCmd = &cobra.Command{
+	rootCommand = &cobra.Command{
 		Use:        cliName,
 		Short:      cliDescription,
 		SuggestFor: []string{"sssn", "sns", "sn"},
 		RunE:       rootCommandFunc,
 	}
-	ssCmd = &cobra.Command{
+	ssCommand = &cobra.Command{
 		Use:   "ss",
 		Short: "ss investigates sockets.",
 		RunE:  ssCommandFunc,
 	}
-	killCmd = &cobra.Command{
+	killCommand = &cobra.Command{
 		Use:   "kill",
 		Short: "kill kills programs using syscall. Make sure to specify the flags to find the program.",
 		RunE:  killCommandFunc,
@@ -110,17 +110,17 @@ func killCommandFunc(cmd *cobra.Command, args []string) error {
 }
 
 func init() {
-	rootCmd.AddCommand(ssCmd)
-	rootCmd.AddCommand(killCmd)
+	rootCommand.AddCommand(ssCommand)
+	rootCommand.AddCommand(killCommand)
 
-	rootCmd.PersistentFlags().StringVarP(&globalFlag.GlobalFilter.Protocol, "protocol", "t", "", "'tcp' or 'tcp6'. Empty lists all protocols.")
-	rootCmd.PersistentFlags().StringVarP(&globalFlag.GlobalFilter.Program, "program", "s", "", "Specify the program. Empty lists all programs.")
-	rootCmd.PersistentFlags().StringVarP(&globalFlag.GlobalFilter.LocalIP, "local-ip", "l", "", "Specify the local IP. Empty lists all local IPs.")
-	rootCmd.PersistentFlags().StringVarP(&globalFlag.GlobalFilter.LocalPort, "local-port", "p", "", "Specify the local port. Empty lists all local ports.")
-	rootCmd.PersistentFlags().StringVarP(&globalFlag.GlobalFilter.RemoteIP, "remote-ip", "r", "", "Specify the remote IP. Empty lists all remote IPs.")
-	rootCmd.PersistentFlags().StringVarP(&globalFlag.GlobalFilter.RemotePort, "remote-port", "m", "", "Specify the remote port. Empty lists all remote ports.")
-	rootCmd.PersistentFlags().StringVarP(&globalFlag.GlobalFilter.State, "state", "a", "", "Specify the state. Empty lists all states.")
-	rootCmd.PersistentFlags().StringVarP(&globalFlag.GlobalFilter.User.Username, "username", "u", "", "Specify the user name. Empty lists all user names.")
+	rootCommand.PersistentFlags().StringVarP(&globalFlag.GlobalFilter.Protocol, "protocol", "t", "", "'tcp' or 'tcp6'. Empty lists all protocols.")
+	rootCommand.PersistentFlags().StringVarP(&globalFlag.GlobalFilter.Program, "program", "s", "", "Specify the program. Empty lists all programs.")
+	rootCommand.PersistentFlags().StringVarP(&globalFlag.GlobalFilter.LocalIP, "local-ip", "l", "", "Specify the local IP. Empty lists all local IPs.")
+	rootCommand.PersistentFlags().StringVarP(&globalFlag.GlobalFilter.LocalPort, "local-port", "p", "", "Specify the local port. Empty lists all local ports.")
+	rootCommand.PersistentFlags().StringVarP(&globalFlag.GlobalFilter.RemoteIP, "remote-ip", "r", "", "Specify the remote IP. Empty lists all remote IPs.")
+	rootCommand.PersistentFlags().StringVarP(&globalFlag.GlobalFilter.RemotePort, "remote-port", "m", "", "Specify the remote port. Empty lists all remote ports.")
+	rootCommand.PersistentFlags().StringVarP(&globalFlag.GlobalFilter.State, "state", "a", "", "Specify the state. Empty lists all states.")
+	rootCommand.PersistentFlags().StringVarP(&globalFlag.GlobalFilter.User.Username, "username", "u", "", "Specify the user name. Empty lists all user names.")
 }
 
 func init() {
@@ -128,7 +128,7 @@ func init() {
 }
 
 func main() {
-	if err := rootCmd.Execute(); err != nil {
+	if err := rootCommand.Execute(); err != nil {
 		fmt.Fprintln(w, err)
 		os.Exit(1)
 	}
