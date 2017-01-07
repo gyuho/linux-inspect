a44f93542bee7409b8cdbd2e9d423cca02bcad08
Gyu-Ho Lee <gyuhox@gmail.com>
2017-01-06 16:46:35 -0800

    *: add 'psn ds' command

--- cmd/psn/ds.go
+++ cmd/psn/ds.go
@@ -1,1 +1,45 @@
 package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/fatih/color"
+	"github.com/gyuho/psn"
+	"github.com/spf13/cobra"
+)
+
+type dsFlags struct {
+}
+
+var (
+	dsCommand = &cobra.Command{
+		Use:   "ds",
+		Short: "Inspects disk statistics",
+		RunE:  dsCommandFunc,
+	}
+	dsCmdFlag = dsFlags{}
+)
+
+func init() {
+}
+
+func dsCommandFunc(cmd *cobra.Command, args []string) error {
+	color.Set(color.FgMagenta)
+	fmt.Fprintf(os.Stdout, "\n'ds' to inspect disk statistics\n\n")
+	color.Unset()
+
+	ds, err := psn.GetDS()
+	if err != nil {
+		return err
+	}
+	hd, rows := psn.ConvertDS(ds...)
+	txt := psn.StringDS(hd, rows, -1)
+	fmt.Print(txt)
+
+	color.Set(color.FgGreen)
+	fmt.Fprintf(os.Stdout, "\nDONE\n")
+	color.Unset()
+
+	return nil
+}
--- cmd/psn/main.go
+++ cmd/psn/main.go
@@ -1,21 +1,3 @@
-// psn provides utilities to investigate OS processes and sockets.
-//
-//	Usage:
-//	  psn [command]
-//
-//	Available Commands:
-//	  process         Investigates processes status
-//	  process-kill    Kills processes
-//	  process-monitor Monitors processes
-//	  socket          Investigates sockets
-//	  socket-kill     Kills sockets
-//	  socket-monitor  Monitors sockets
-//
-//	Flags:
-//	  -h, --help[=false]: help for psn
-//
-//	Use "psn [command] --help" for more information about a command.
-//
 package main
 
 import (
@@ -28,18 +10,13 @@ import (
 var (
 	Command = &cobra.Command{
 		Use:        "psn",
-		Short:      "psn provides utilities to investigate OS processes and sockets.",
+		Short:      "psn inspects Linux processes, sockets (ps, ss, netstat).",
 		SuggestFor: []string{"pssn", "psns", "snp"},
 	}
 )
 
 func init() {
-	Command.AddCommand(processCommand)
-	Command.AddCommand(processKillCommand)
-	Command.AddCommand(processMonitorCommand)
-	Command.AddCommand(socketCommand)
-	Command.AddCommand(socketKillCommand)
-	Command.AddCommand(socketMonitorCommand)
+	Command.AddCommand(dsCommand)
 }
 
 func init() {
