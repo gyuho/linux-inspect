a623bcfe05ef82cf65f39fdca315c1255c7b2902
Gyu-Ho Lee <gyuhox@gmail.com>
2017-03-15 12:53:14 -0700

    pkg/timeutil: add
    Signed-off-by: Gyu-Ho Lee <gyuhox@gmail.com>

--- pkg/timeutil/timeutil.go
+++ pkg/timeutil/timeutil.go
@@ -1,7 +1,12 @@
 // Package timeutil implements time utilities.
 package timeutil
 
-import "time"
+import (
+	"strings"
+	"time"
+
+	humanize "github.com/dustin/go-humanize"
+)
 
 func NowPST() time.Time {
 	tzone, err := time.LoadLocation("America/Los_Angeles")
@@ -9,4 +14,20 @@ func NowPST() time.Time {
 		return time.Now()
 	}
 	return time.Now().In(tzone)
+}
+
+func HumanizeDurationMs(ms uint64) string {
+	s := humanize.Time(time.Now().Add(-1 * time.Duration(ms) * time.Millisecond))
+	if s == "now" {
+		s = "0 seconds"
+	}
+	return strings.TrimSpace(strings.Replace(s, " ago", "", -1))
+}
+
+func HumanizeDurationSecond(sec uint64) string {
+	s := humanize.Time(time.Now().Add(-1 * time.Duration(sec) * time.Second))
+	if s == "now" {
+		s = "0 seconds"
+	}
+	return strings.TrimSpace(strings.Replace(s, " ago", "", -1))
 }
