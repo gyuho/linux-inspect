5bce643dd777efc64aab658872894893f7d97783
Gyu-Ho Lee <gyuhox@gmail.com>
2017-05-09 16:28:06 -0700

    scripts: add 'update-schema.sh'
    Signed-off-by: Gyu-Ho Lee <gyuhox@gmail.com>

---
+++ scripts/update-schema.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e
+
+if ! [[ "$0" =~ "scripts/update-schema.sh" ]]; then
+	echo "must be run from repository root"
+	exit 255
+fi
+
+go run ./cmd/generate-df/main.go
+go run ./cmd/generate-etc/main.go
+go run ./cmd/generate-proc/main.go
+go run ./cmd/generate-top/main.go
